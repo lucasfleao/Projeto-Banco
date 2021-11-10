7769f43f79923588bf8ad7bf7bc304027bc4fcf8
Lucas Leão <lucasfleao94@gmail.com>
2021-11-10 12:25:06 -0300

    Removendo elses e utilizando returns para simplificar códigos

--- src/Conta.php
+++ src/Conta.php
@@ -4,24 +4,39 @@ class Conta
 {
     public string $cpfTitular;
     public string $nomeTitular;
-    public float $saldo;
+    public float $saldo = 0;
 
     public function sacar(float $valorASacar) :void
     {
         if ($valorASacar > $this->saldo) {
             echo "Saldo Indisponível";
+            return;
         }
-        else {
-            $this->saldo -= $valorASacar;
-        }
+        
+        $this->saldo -= $valorASacar;
+        
     } 
 
     public function depositar(float $ValorADepositar):void
     {
-        if ($ValorADepositar > 0) {
-            $this->saldo += $ValorADepositar;
-        } else {
+        if ($ValorADepositar < 0) {
             echo "O Valor precisa ser positivo";
+            return;
         }
+            
+        $this->saldo += $ValorADepositar;
+        
+    }
+
+    public function transferir (float $valorATransferir, Conta $contaDestino):void
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo "Saldo Indisponível";
+            return;
+        }
+
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
+        
     }
 }
