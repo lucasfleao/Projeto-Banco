8d3cbd1144bfc6455192598dba53fef61a743067
Lucas Leão <lucasfleao94@gmail.com>
2021-11-10 11:54:51 -0300

    Adicionando Métodos e utilizando variavel this

--- src/Conta.php
+++ src/Conta.php
@@ -5,4 +5,14 @@ class Conta
     public string $cpfTitular;
     public string $nomeTitular;
     public float $saldo;
+
+    public function sacar(float $valorASacar) :void
+    {
+        if ($valorASacar > $this->saldo) {
+            echo "Saldo Indisponível";
+        }
+        else {
+            $this->saldo -= $valorASacar;
+        }
+    }
 }
